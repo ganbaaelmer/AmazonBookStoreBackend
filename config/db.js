2316daa765d0e85123a0725611aa177a20b3007c
ganbaaelmer <ganbaa_elmer@yahoo.com>
2024-07-02 16:58:49 +0800

    mongoose tohirgoo saijruulsan

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    // useUnifiedTopology: true,
   });
 
   console.log(
